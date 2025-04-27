4539732a41f0085126d157700e6b9b5a683a0fd1
mgsrood <mgsrood@gmail.com>
2025-04-27 20:20:43 +0200

    Betere verwerking fouten / sync update

--- sql_script/afas/afas.py
+++ sql_script/afas/afas.py
@@ -22,102 +22,104 @@ def afas(connection_string, config_manager):
         config_manager: Instantie van ConfigManager
     """
     
-    # Klant configuratie
-    errors_occurred = False
-    nieuwe_laatste_sync = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    
-    # Configuraties ophalen
-    config_dict = config_manager.get_configurations(connection_string)
-    if config_dict is not None:
-        laatste_sync = config_dict["Laatste_sync"]
-
-    else:
-        errors_occurred = True
-        return False
-
-    # Ophalen tabel configuratie gegevens
-    table_config_dict = config_manager.get_table_configurations(connection_string)
-    if table_config_dict is None:
-        errors_occurred = True
-        return False
-
-    # Ophalen omgevings configuratie gegevens
-    environment_dict = config_manager.create_environment_dict(connection_string)
-    if environment_dict is None:
-        errors_occurred = True
-        return False
-
-    # Endpoint loop
-    for klant, (omgeving_id, api_string, token, status) in environment_dict.items():
-        if status == 0:
-            logging.info(f"Overslaan van GET Requests voor omgeving: {klant}")
-            continue
+    try:
+        # Klant configuratie
+        errors_occurred = False
+        nieuwe_laatste_sync = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
         
-        # Stel de logging context in voor deze omgeving
-        config_manager.set_logging_context(administratiecode=omgeving_id, tabel=None)
-        
-        for table, status in table_config_dict.items():
+        # Configuraties ophalen
+        config_dict = config_manager.get_configurations(connection_string)
+        if config_dict is not None:
+            laatste_sync = config_dict["Laatste_sync"]
+
+        else:
+            errors_occurred = True
+            return False
+
+        # Ophalen tabel configuratie gegevens
+        table_config_dict = config_manager.get_table_configurations(connection_string)
+        if table_config_dict is None:
+            errors_occurred = True
+            return False
+
+        # Ophalen omgevings configuratie gegevens
+        environment_dict = config_manager.create_environment_dict(connection_string)
+        if environment_dict is None:
+            errors_occurred = True
+            return False
+
+        # Endpoint loop
+        for klant, (omgeving_id, api_string, token, status) in environment_dict.items():
             if status == 0:
-                logging.info(f"Overslaan van GET Requests voor endpoint: {table} | {klant}")
+                logging.info(f"Overslaan van GET Requests voor omgeving: {klant}")
                 continue
-        
-            # Stel de logging context in voor deze tabel, behoud de administratiecode
-            config_manager.set_logging_context(administratiecode=omgeving_id, tabel=table)
             
-            # Connector ophalen
-            sync_manager = SyncFormatManager(config_manager)
-            connectors = sync_manager.return_connectors(laatste_sync)
+            # Stel de logging context in voor deze omgeving
+            config_manager.set_logging_context(administratiecode=omgeving_id, tabel=None)
             
-            if table in connectors:
-                connector = connectors[table]
+            for table, status in table_config_dict.items():
+                if status == 0:
+                    logging.info(f"Overslaan van GET Requests voor endpoint: {table} | {klant}")
+                    continue
             
-                # Uitvoeren GET Request
-                logging.info(f"Start GET Requests voor tabel: {table} | {klant}")
-                df, error = execute_get_request(api_string, token, connector, klant, table)
+                # Stel de logging context in voor deze tabel, behoud de administratiecode
+                config_manager.set_logging_context(administratiecode=omgeving_id, tabel=table)
                 
-                if error:
-                    errors_occurred = True
+                # Connector ophalen
+                sync_manager = SyncFormatManager(config_manager)
+                connectors = sync_manager.return_connectors(laatste_sync)
+                
+                if table in connectors:
+                    connector = connectors[table]
+                
+                    # Uitvoeren GET Request
+                    logging.info(f"Start GET Requests voor tabel: {table} | {klant}")
+                    df, error = execute_get_request(api_string, token, connector, klant, table)
                     
-                if df is None or df.empty:
-                    # Als de DataFrame leeg is, sla deze omgeving/tabel over
-                    logging.warning(f"Overslaan van verdere verwerking voor tabel {table} omdat er geen data is.")
-                    continue
-                pd.set_option('display.max_columns', None)
-                print(f"Table: {table}, df: {df.head()}")
-                
-                # Omgeving ID toevoegen
-                df = add_environment_id(df, omgeving_id)
-                
-                if df is None:
-                    errors_occurred = True
-                    continue
-                
-                # Type conversie toepassen
-                df_converted = apply_type_conversion(df, table)
-                if df_converted is None:
-                    continue
+                    if error:
+                        errors_occurred = True
+                        
+                    if df is None or df.empty:
+                        # Als de DataFrame leeg is, sla deze omgeving/tabel over
+                        logging.warning(f"Overslaan van verdere verwerking voor tabel {table} omdat er geen data is.")
+                        continue
+                    pd.set_option('display.max_columns', None)
+                    print(f"Table: {table}, df: {df.head()}")
+                    
+                    # Omgeving ID toevoegen
+                    df = add_environment_id(df, omgeving_id)
+                    
+                    if df is None:
+                        errors_occurred = True
+                        continue
+                    
+                    # Type conversie toepassen
+                    df_converted = apply_type_conversion(df, table)
+                    if df_converted is None:
+                        continue
 
-                # Rijen verwijderen
-                apply_table_clearing(connection_string, table, omgeving_id, laatste_sync)
-                
-                # Rijen toevoegen
-                succes = apply_table_writing(df_converted, connection_string, table, laatste_sync)
+                    # Rijen verwijderen
+                    apply_table_clearing(connection_string, table, omgeving_id, laatste_sync)
+                    
+                    # Rijen toevoegen
+                    succes = apply_table_writing(df_converted, connection_string, table, laatste_sync)
 
-                if succes is False:
-                    errors_occurred = True
-                    continue
-                
+                    if succes is False:
+                        errors_occurred = True
+                        continue
+                    
+            # Logging van afronding 
+            logging.info(f"GET Requests succesvol afgerond voor: {klant} | {omgeving_id}")
+        
         # Logging van afronding 
-        logging.info(f"GET Requests succesvol afgerond voor: {klant} | {omgeving_id}")
-    
-    # Logging van afronding 
-    logging.info(f"Alle divisies succesvol verwerkt voor klant {klant}")
-    
-    # Laatste sync en rapportage jaar bijwerken
-    if errors_occurred is False:
-        config_manager.update_last_sync(connection_string, nieuwe_laatste_sync)
-        config_manager.update_reporting_year(connection_string)
-        logging.info(f"Script succesvol afgerond")
         logging.info(f"Alle divisies succesvol verwerkt voor klant {klant}")
-    else:
+        
+        # Laatste sync en rapportage jaar bijwerken
+        if errors_occurred is False:
+            config_manager.update_last_sync(connection_string, nieuwe_laatste_sync)
+            config_manager.update_reporting_year(connection_string)
+            logging.info(f"Script succesvol afgerond")
+            logging.info(f"Alle divisies succesvol verwerkt voor klant {klant}")
+            
+    except Exception as e:
         logging.error(f"Fout bij het verwerken van de divisies voor klant {klant}, laatste sync en rapportage jaar niet bijgewerkt")
--- sql_script/exact/exact.py
+++ sql_script/exact/exact.py
@@ -24,160 +24,163 @@ def exact(connection_string, config_manager):
         connection_string: Connectiestring voor de database
         config_manager: Instantie van ConfigManager
     """
-    # Environment configuratie
-    env_config = EnvConfig()
-    env_config_dict = env_config.get_database_config()
-    
-    # Klant configuratie
-    errors_occurred = False
-    nieuwe_laatste_sync = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    
-    # Configuraties ophalen
-    config_dict = config_manager.get_configurations(connection_string)
-    if config_dict is not None:
-        laatste_sync = config_dict["Laatste_sync"]
-        reporting_year = config_dict["ReportingYear"]
-    else:
-        errors_occurred = True
-        return False
-
-    # Ophalen tabel configuratie gegevens
-    table_config_dict = config_manager.get_table_configurations(connection_string)
-    if table_config_dict is None:
-        errors_occurred = True
-        return False
     
     try:
-        # Basis URL voor Exact Online API
-        url = "https://start.exactonline.nl/api/v1/"
+        # Environment configuratie
+        env_config = EnvConfig()
+        env_config_dict = env_config.get_database_config()
         
-        # Huidige divisiecode ophalen
-        current_division_code = current_division_call(config_manager, url, connection_string)
+        # Klant configuratie
+        errors_occurred = False
+        nieuwe_laatste_sync = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
         
-        if current_division_code is None:
-            logging.error("Kon geen huidige divisie ophalen")
+        # Configuraties ophalen
+        config_dict = config_manager.get_configurations(connection_string)
+        if config_dict is not None:
+            laatste_sync = config_dict["Laatste_sync"]
+            reporting_year = config_dict["ReportingYear"]
+        else:
+            errors_occurred = True
+            return False
+
+        # Ophalen tabel configuratie gegevens
+        table_config_dict = config_manager.get_table_configurations(connection_string)
+        if table_config_dict is None:
+            errors_occurred = True
+            return False
+        
+        try:
+            # Basis URL voor Exact Online API
+            url = "https://start.exactonline.nl/api/v1/"
+            
+            # Huidige divisiecode ophalen
+            current_division_code = current_division_call(config_manager, url, connection_string)
+            
+            if current_division_code is None:
+                logging.error("Kon geen huidige divisie ophalen")
+                return False
+            
+            # Divisies ophalen
+            divisions_df, errors = execute_divisions_call(config_manager, url, connection_string, current_division_code)
+
+            if errors:
+                logging.error("Fout bij ophalen divisies")
+                errors_occurred = True
+                return False
+                
+            if divisions_df is None or divisions_df.empty:
+                logging.warning("Geen relevante divisies gevonden")
+                return False
+            
+            # Kolom mapping toepassen
+            df_transformed = apply_column_mapping(divisions_df, "Divisions", current_division_code)
+            
+            if df_transformed is None:
+                errors_occurred = True
+                return False
+            
+            # Type conversie toepassen
+            df_converted = apply_type_conversion(df_transformed, "Divisions")
+            
+            if df_converted is None:
+                errors_occurred = True
+                return False
+            
+            # Legen Divisions tabel
+            apply_table_clearing(connection_string, "Divisions")
+            
+            # Divisions tabel vullen
+            apply_table_writing(connection_string, df_converted, "Divisions")
+            
+        except Exception as e:
+            logging.error(f"Onverwachte fout in Exact verwerking: {e}")
+            errors_occurred = True
             return False
         
         # Divisies ophalen
-        divisions_df, errors = execute_divisions_call(config_manager, url, connection_string, current_division_code)
+        division_codes = config_manager.get_division_codes(connection_string)
+        
+        if not division_codes:
+            logging.error("Geen divisie codes kunnen ophalen")
+            errors_occurred = True
+            return False
 
-        if errors:
-            logging.error("Fout bij ophalen divisies")
-            errors_occurred = True
-            return False
+        # Loop door alle divisie codes
+        for division_name, division_code in division_codes.items():
+            logging.info(f"Verwerken divisie: {division_name} ({division_code})")
             
-        if divisions_df is None or divisions_df.empty:
-            logging.warning("Geen relevante divisies gevonden")
-            return False
-        
-        # Kolom mapping toepassen
-        df_transformed = apply_column_mapping(divisions_df, "Divisions", current_division_code)
-        
-        if df_transformed is None:
-            errors_occurred = True
-            return False
-        
-        # Type conversie toepassen
-        df_converted = apply_type_conversion(df_transformed, "Divisions")
-        
-        if df_converted is None:
-            errors_occurred = True
-            return False
-        
-        # Legen Divisions tabel
-        apply_table_clearing(connection_string, "Divisions")
-        
-        # Divisions tabel vullen
-        apply_table_writing(connection_string, df_converted, "Divisions")
-        
+            # Stel de logging context in voor deze divisie
+            config_manager.set_logging_context(administratiecode=division_code, tabel=None)
+            
+            # Basis URL voor Exact Online API met Divisie code extensie
+            url = f"https://start.exactonline.nl/api/v1/{division_code}/"
+            
+            # Synchroinsatie vorm bepalen
+            sync_manager = SyncFormatManager(config_manager)
+            
+            try:
+                endpoints = sync_manager.define_sync_format(laatste_sync, reporting_year)
+                # Gebruik de endpoints voor verdere verwerking
+                
+            except Exception as e:
+                config_manager.logger.error(f"Fout tijdens bepalen sync format: {str(e)}")
+                errors_occurred = True
+                continue
+            
+            # Endpoint loop
+            for tabel, endpoint in endpoints.items():
+                for table, status in table_config_dict.items():
+                    if table == tabel:
+                        if status == 0:
+                            logging.info(f"Overslaan van GET Requests voor endpoint: {tabel} | {division_name} ({division_code})")
+                            continue
+                        
+                        else:
+                            # Stel de logging context in voor deze tabel, behoud de administratiecode
+                            config_manager.set_logging_context(administratiecode=division_code, tabel=tabel)
+                            
+                            # Uitvoeren GET Request
+                            df, error = execute_get_request(config_manager, division_code, url, endpoint, connection_string, tabel, division_name)
+                            
+                            if error:
+                                errors_occurred = True
+                                continue
+                            
+                            if df is None or df.empty:
+                                logging.warning(f"Geen data opgehaald voor tabel: {tabel} | {division_name} ({division_code})")
+                                continue
+                            
+                            # Maak een DataTransformer instantie
+                            transformer = DataTransformer(config_manager)
+                            
+                            # Mogelijke appending functies toepassen
+                            df_appended = transformer.transform_data(df, tabel, division_code, division_name)
+                            
+                            # Kolom mapping toepassen
+                            df_transformed = apply_column_mapping(df_appended, tabel, division_code)
+                            
+                            # Type conversie toepassen
+                            df_converted = apply_type_conversion(df_transformed, tabel)
+
+                            # Rijen verwijderen
+                            apply_table_clearing(connection_string, table, division_code, reporting_year, laatste_sync)
+                            
+                            # Rijen toevoegen
+                            succes = apply_table_writing(connection_string, df_converted, tabel, laatste_sync)
+                            
+                            if succes is False:
+                                errors_occurred = True
+                                continue
+            
+            # Logging van afronding 
+            logging.info(f"GET Requests succesvol afgerond voor divisie: {division_name} ({division_code})")
+            
+            # Laatste sync en rapportage jaar bijwerken
+            if errors_occurred is False:
+                config_manager.update_last_sync(connection_string, nieuwe_laatste_sync)
+                config_manager.update_reporting_year(connection_string)
+                logging.info(f"Script succesvol afgerond")
+                logging.info(f"Alle divisies succesvol verwerkt voor klant {env_config_dict['klant_naam']}")
+
     except Exception as e:
-        logging.error(f"Onverwachte fout in Exact verwerking: {e}")
-        errors_occurred = True
-        return False
-    
-    # Divisies ophalen
-    division_codes = config_manager.get_division_codes(connection_string)
-    
-    if not division_codes:
-        logging.error("Geen divisie codes kunnen ophalen")
-        errors_occurred = True
-        return False
-
-    # Loop door alle divisie codes
-    for division_name, division_code in division_codes.items():
-        logging.info(f"Verwerken divisie: {division_name} ({division_code})")
-        
-        # Stel de logging context in voor deze divisie
-        config_manager.set_logging_context(administratiecode=division_code, tabel=None)
-        
-        # Basis URL voor Exact Online API met Divisie code extensie
-        url = f"https://start.exactonline.nl/api/v1/{division_code}/"
-        
-        # Synchroinsatie vorm bepalen
-        sync_manager = SyncFormatManager(config_manager)
-        
-        try:
-            endpoints = sync_manager.define_sync_format(laatste_sync, reporting_year)
-            # Gebruik de endpoints voor verdere verwerking
-            
-        except Exception as e:
-            config_manager.logger.error(f"Fout tijdens bepalen sync format: {str(e)}")
-            errors_occurred = True
-            continue
-        
-        # Endpoint loop
-        for tabel, endpoint in endpoints.items():
-            for table, status in table_config_dict.items():
-                if table == tabel:
-                    if status == 0:
-                        logging.info(f"Overslaan van GET Requests voor endpoint: {tabel} | {division_name} ({division_code})")
-                        continue
-                    
-                    else:
-                        # Stel de logging context in voor deze tabel, behoud de administratiecode
-                        config_manager.set_logging_context(administratiecode=division_code, tabel=tabel)
-                        
-                        # Uitvoeren GET Request
-                        df, error = execute_get_request(config_manager, division_code, url, endpoint, connection_string, tabel, division_name)
-                        
-                        if error:
-                            errors_occurred = True
-                            continue
-                        
-                        if df is None or df.empty:
-                            logging.warning(f"Geen data opgehaald voor tabel: {tabel} | {division_name} ({division_code})")
-                            continue
-                        
-                        # Maak een DataTransformer instantie
-                        transformer = DataTransformer(config_manager)
-                        
-                        # Mogelijke appending functies toepassen
-                        df_appended = transformer.transform_data(df, tabel, division_code, division_name)
-                        
-                        # Kolom mapping toepassen
-                        df_transformed = apply_column_mapping(df_appended, tabel, division_code)
-                        
-                        # Type conversie toepassen
-                        df_converted = apply_type_conversion(df_transformed, tabel)
-
-                        # Rijen verwijderen
-                        apply_table_clearing(connection_string, table, division_code, reporting_year, laatste_sync)
-                        
-                        # Rijen toevoegen
-                        succes = apply_table_writing(connection_string, df_converted, tabel, laatste_sync)
-                        
-                        if succes is False:
-                            errors_occurred = True
-                            continue
-        
-        # Logging van afronding 
-        logging.info(f"GET Requests succesvol afgerond voor divisie: {division_name} ({division_code})")
-    
-    # Laatste sync en rapportage jaar bijwerken
-    if errors_occurred is False:
-        config_manager.update_last_sync(connection_string, nieuwe_laatste_sync)
-        config_manager.update_reporting_year(connection_string)
-        logging.info(f"Script succesvol afgerond")
-        logging.info(f"Alle divisies succesvol verwerkt voor klant {env_config_dict['klant_naam']}")
-    else:
         logging.error(f"Fout bij het verwerken van de divisies voor klant {env_config_dict['klant_naam']}, laatste sync en rapportage jaar niet bijgewerkt")
